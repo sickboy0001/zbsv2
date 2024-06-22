3cbfde36c770661336af4adb77ae0f504709a593
tfujiki <syunjyu0001@gmail.com>
2024-06-23 05:24:27 +0900

    ♻️ testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>indexpage</title>
+      </head>
+
       <body className={inter.className}>{children}</body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer>
+      <div>CopyRight xxxx</div>
+    </footer>
+  );
+}
